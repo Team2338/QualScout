0b7ba442dd2c916451a7366380909103da872e47
Geet Gambhir <geetygam@gmail.com>
2023-01-11 20:28:11 -0600

    Add header in landing page, bunch of deps for react scripts

--- src/Components/landing-page/LandingPage.jsx
+++ src/Components/landing-page/LandingPage.jsx
@@ -6,3 +6,35 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 
 import './LandingPage.scss';
+
+
+class LandingPage extends React.Component {
+
+
+
+
+
+
+render() {
+    return ( /* error here */
+        <div className='wrapper'>
+            <div className='header'>
+                <div className='left'>
+                    <div>placeholder text for image</div>
+                </div>
+                <h1 className='Title'>2338 Gear It Forward</h1>
+                <span>
+                    
+                    Future Language Button 
+                    
+                    </span> 
+            </div>
+        </div>
+    )
+}
+
+}
+
+
+
+export default LandingPage;
