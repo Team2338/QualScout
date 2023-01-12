87de1f1a0ff680425ecfc697e49298cbf45e8984
Geet Gambhir <geetygam@gmail.com>
2023-01-11 19:24:43 -0600

    Initial boilerplate importing  for landing page

---
+++ src/Components/landing-page/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from  '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { InputAdornment } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
+import './LandingPage.scss';
