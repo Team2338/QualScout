8b61a698764d41b1d467c49997eaba5d048132b3
Geet Gambhir <geetygam@gmail.com>
2023-01-14 13:14:23 -0600

    initial changes

---
+++ src/Components/auto-page/Auto.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button  from '@mui/material/Button';
+
+import './Auto.scss';
+
+
+class Auto extends React.Component { 
+    /*
+    Points for auto is as follows: 
+    Mobile: 3 
+    bottom row: 3 
+    middle row: 4
+    top row: 6
+    Docked not engaged: 8 (1 robot max)
+    Docked Engaged: 12 (1 robot max)
+
+
+
+
+    */
+}
+
+
+
+export default Auto;
