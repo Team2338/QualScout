bc79a3370a0e5565f23fb66d6a63ce18b8b9eb73
Douglas Kampschroer <DougK578@outlook.com>
2023-01-14 14:05:22 -0600

    Added routes

---
+++ src/Components/auto-page/Auto.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+class Auto extends React.Component {
+
+}
+export default Auto;
---
+++ src/Components/landing-page/LandingPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+class LandingPage extends React.Component {
+
+}
+export default LandingPage;
