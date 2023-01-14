1dde0a5ea05aada0778cadaadafec99587699fa8
Geet Gambhir <geetygam@gmail.com>
2023-01-14 13:07:10 -0600

    initial changes

---
+++ src/Components/teleop-page/Teleop.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from '@mui/material/Button'
+
+
+import './Teleop.scss';
+
+
+
+class Teleop extends React.Component {
+    render() {
+        return ( 
+         <h3>Teleop</h3>
+         /* 3 types of game pieces: Hybrid, cube, and cone
+            Teleop scores:
+            bottom row: 2 
+            middle row: 3
+            top row: 5
+            Link bonus:5
+            Docked unengaged: 6
+            Docked engaged: 10 
+            Park: 2
+
+         */
+        )
+    }
+}
+
+
+
+export default Teleop;
