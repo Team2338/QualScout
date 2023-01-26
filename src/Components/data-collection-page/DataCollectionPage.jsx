28cc5c6bde43a6ae6143b729b90fb33892f16a49
Doug-Kamp <75461735+Doug-Kamp@users.noreply.github.com>
2023-01-25 20:52:40 -0600

    Created the match info page which gets the team and match numbers.

--- src/Components/data-collection-page/DataCollectionPage.jsx
+++ src/Components/data-collection-page/DataCollectionPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@mui/material/Button'
+import MatchInfo from '../match-information/MatchInformation.jsx'
 import Auto from '../auto-page/Auto.jsx';
 import Teleop from'../teleop-page/Teleop.jsx'
 
@@ -8,6 +9,7 @@ class DataCollectionPage extends React.Component {
     render() {
         return (
           <div>
+            <MatchInfo />
             <Auto />
             <Teleop />
             <Button variant='contained' href='/'>Back</Button>
